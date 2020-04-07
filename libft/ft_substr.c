bda4a0d15e46d92de58058a9e5b1fb08b50ce400
yujo <yujo@student.42seoul.kr>
2020-04-07 19:50:33 +0900

    modified substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,18 +6,33 @@
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 16:20:43 by yujo              #+#    #+#             */
-/*   Updated: 2020/02/28 16:51:53 by yujo             ###   ########.fr       */
+/*   Updated: 2020/04/07 19:33:55 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int	start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*result;
+	size_t	count;
+	size_t	size;
+	char	*tab;
 
-	if (!(result = malloc(++len)))
-		return (0);
-	ft_strlcpy(result, s + start, len);
-	return (result);
+	count = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	while (count < len)
+	{
+		tab[count] = s[start + count];
+		count++;
+	}
+	tab[count] = '\0';
+	return (tab);
 }
