bd28aaca454f260d9aaa6c7141a5418669f843c9
yujo <yujo@student.42seoul.kr>
2020-06-30 03:14:13 +0900

    make GNL Basic Template

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/29 21:31:02 by yujo              #+#    #+#             */
+/*   Updated: 2020/06/30 02:34:22 by yujo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
+char    *ft_strnew(size_t size);
+void	ft_strdel(char **str);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+int     ft_new_line(char **save_line, char **line, int fd, int ret);
+int     get_next_line(int fd, char **line);
+
+#endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/29 21:30:59 by yujo              #+#    #+#             */
+/*   Updated: 2020/06/30 03:11:26 by yujo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc(size + 1);
+	if (str == NULL)
+		return (NULL);
+	while (i < size)
+		str[i++] = 0;
+	str[size] = '\0';
+	return (str);
+}
+
+void	ft_strdel(char **str)
+{
+	if (str)
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return ;
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (unsigned char)c)
+	{
+		if (*s == 0)
+			return (0);
+		s++;
+	}
+	return ((char*)s);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (*dst && i < size)
+	{
+		i++;
+		dst++;
+	}
+	while (*src && i + 1 < size)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+		i++;
+	}
+	if (i < size)
+		*dst = 0;
+	while (*src)
+	{
+		i++;
+		src++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t i;
+	size_t len;
+
+	i = 0;
+	if (dst == 0)
+		return (0);
+	if (size > 0)
+	{
+		while (i < size - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	len = 0;
+	while (src[len])
+		len++;
+	return (len);
+}
