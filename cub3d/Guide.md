c81e309af596e1ac42d1d95b7f7eb736a2182016
yujo <yujo@student.42seoul.kr>
2020-08-28 20:55:30 +0900

    directory cleanup

---
+++ cub3d/Guide.md
@@ -0,0 +1,11 @@
+## cub3d Guide
+
+### dir 구조
+
+cub3d - utils - gnl
+              - libft
+              - mlx
+              - keymap.h
+
+      - src   - draw_map - textures
+                         - src
--- cub3d/src/cub3d.h
+++ cub3d/src/cub3d.h
@@ -1,13 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.c                                            :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yujo <yujo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/26 18:56:59 by yujo              #+#    #+#             */
-/*   Updated: 2020/08/26 18:57:18 by yujo             ###   ########.fr       */
+/*   Created: 2020/08/26 18:57:24 by yujo              #+#    #+#             */
+/*   Updated: 2020/08/28 20:53:31 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#ifndef CUB3D_HEADER
+# define CUB3D_HEADER
+
+# include "../utils/keymap.h"
+# include "../utils/libft/libft.h"
+# include "../utils/mlx_beta/mlx.h"
+# include "../utils/gnl/get_next_line.h
+
+# include <math.h>
+# include <fcntl.h>
+
+#endif
