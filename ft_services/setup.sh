c108f9b3ba1e0311b353c4ef885700a9632cfdae
yujo <yujo@student.42seoul.kr>
2020-10-15 20:12:59 +0900

    ok

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -6,8 +6,17 @@
 #    By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/23 10:51:32 by yujo              #+#    #+#              #
-#    Updated: 2020/09/24 15:16:52 by yujo             ###   ########.fr        #
+#    Updated: 2020/09/26 13:58:26 by yujo             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+
+echo "metalLB manifest"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+cd srcs/metallb
+kubectl apply -f config.yam
+
+# echo "dashboard activate"
 # https://minikube.sigs.k8s.io/docs/handbook/dashboard/
--- ft_services/srcs/clean.sh
+++ ft_services/srcs/clean.sh
@@ -6,7 +6,9 @@
 #    By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/23 13:14:47 by yujo              #+#    #+#              #
-#    Updated: 2020/09/26 10:14:31 by yujo             ###   ########.fr        #
+#    Updated: 2020/09/26 17:48:31 by yujo             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+echo "metalLB manifest delete"
+kubectl delete -f https://raw.githubsercontent.com/metallb/v0.9.3/manifest/namespace.yaml
